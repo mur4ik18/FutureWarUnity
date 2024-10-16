c6b6bfc24e1fe8cfa8f5ca1ec4a0d282305b4024
Oleksandr KOTOV <u2107883@ensimlan.univ-lemans.fr>
2024-10-16 12:18:46 +0200

    Add hand detection and pointing buy hands

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -8,12 +8,18 @@ public class Move : MonoBehaviour
 {
     public InputActionReference move;
 
-
     [Header("Movement")]
     public float moveSpeed;
+    public float groundDrag;
 
     Vector3 moveDirection;
     Rigidbody rb;
+
+
+    [Header("Ground check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -22,23 +28,39 @@ public class Move : MonoBehaviour
         rb.freezeRotation = true;
 
         move.action.Enable();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+
         Vector2 verticalInput = move.action.ReadValue<Vector2>();
         moveDirection = transform.forward * -(verticalInput.x);
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Acceleration);
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         //transform.localEulerAngles = Vector3.zero;
 
-
-        
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
     }
 
     private void OnDisable()
     {
         move.action.Disable();
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitVel.x, rb.velocity.y, limitVel.z);
+        }
     }
 }
---
+++ Assets/Scripts/RightArm.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightArm : MonoBehaviour
+{
+    // left controller for getting direction
+    public Transform rightArm;
+    // limitation of Y angle 
+    public float verticalRotationLimit = 0.2f;
+    // To show data
+    public Quaternion orientation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Here I get controller rotation
+        orientation = rightArm.rotation;
+        // To curb y rotation
+        orientation.y = Mathf.Clamp(orientation.y, -verticalRotationLimit, verticalRotationLimit);
+        // copy rotation with my curbed values
+        transform.rotation = orientation;
+    }
+}
